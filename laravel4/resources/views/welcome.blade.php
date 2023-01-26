995c40f710527e3869dc573a5f369704f39eed7f
Giangabriele De Luca <giangabriele.deluca@gmail.com>
2023-01-26 12:40:00 +0100

    nuovo progetto in Laraverl4

---
+++ laravel4/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-header>
+        My Favourite Cars
+    </x-header>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+</x-layout>
